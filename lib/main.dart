6b2b2e4c29539fd401f06257ccf1190c726b4c45
kisalay78 <kisalaykar475@gmail.com>
2026-03-14 14:36:20 +0530

    initial project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/auth/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -13,21 +21,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AI Interview Simulator',
-
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          elevation: 0,
-        ),
       ),
-
       home: const LoginScreen(),
-
-      routes: {
-        '/login': (context) => const LoginScreen(),
-      },
     );
   }
 }
