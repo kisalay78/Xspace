7a29af1fb9b420331fe9a605034c11dd8c52d9bb
kisalay78 <kisalaykar475@gmail.com>
2026-03-16 18:41:44 +0530

    initial project setup

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,16 +1,73 @@
 import 'package:flutter/material.dart';
 import '../dashboard/difficulty_screen.dart';
 import 'signup_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> loginUser() async {
+  try {
+
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    );
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const DifficultyScreen(),
+      ),
+    );
+
+  } on FirebaseAuthException catch (e) {
+
+    String message = "";
+
+    if (e.code == 'user-not-found') {
+      message = "User not found. Please Sign Up first.";
+    } 
+    else if (e.code == 'wrong-password') {
+      message = "Incorrect password.";
+    } 
+    else if (e.code == 'invalid-email') {
+      message = "Invalid email address.";
+    } 
+    else {
+      message = e.message ?? "Login failed";
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+}
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Container(
         width: double.infinity,
         padding: const EdgeInsets.all(20),
+
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [Color(0xff1E3A8A), Color(0xff2563EB)],
@@ -18,6 +75,7 @@ class LoginScreen extends StatelessWidget {
             end: Alignment.bottomCenter,
           ),
         ),
+
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -42,6 +100,7 @@ class LoginScreen extends StatelessWidget {
             const SizedBox(height: 40),
 
             TextField(
+              controller: emailController,
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
@@ -55,6 +114,7 @@ class LoginScreen extends StatelessWidget {
             const SizedBox(height: 20),
 
             TextField(
+              controller: passwordController,
               obscureText: true,
               decoration: InputDecoration(
                 filled: true,
@@ -71,18 +131,14 @@ class LoginScreen extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               height: 50,
+
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.black,
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const DifficultyScreen(),
-                    ),
-                  );
-                },
+
+                onPressed: loginUser,
+
                 child: const Text(
                   "Login",
                   style: TextStyle(fontSize: 18),
@@ -93,31 +149,36 @@ class LoginScreen extends StatelessWidget {
             const SizedBox(height: 20),
 
             Row(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: [
-    const Text(
-      "Don't have an account? ",
-      style: TextStyle(color: Colors.white),
-    ),
-    GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const SignupScreen(),
-          ),
-        );
-      },
-      child: const Text(
-        "Sign Up",
-        style: TextStyle(
-          color: Colors.yellow,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ),
-  ],
-)
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                const Text(
+                  "Don't have an account? ",
+                  style: TextStyle(color: Colors.white),
+                ),
+
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SignupScreen(),
+                      ),
+                    );
+                  },
+
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+
+              ],
+            )
+
           ],
         ),
       ),
--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -1,22 +1,79 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
-class SignupScreen extends StatelessWidget {
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> signUpUser() async {
+    try {
+
+      if (passwordController.text != confirmPasswordController.text) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Passwords do not match")),
+        );
+        return;
+      }
+
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Signup Successful")),
+      );
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString())),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: const Text("Sign Up"),
         centerTitle: true,
       ),
+
       body: Padding(
         padding: const EdgeInsets.all(20),
+
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
 
             TextField(
+              controller: nameController,
               decoration: InputDecoration(
                 hintText: "Full Name",
                 border: OutlineInputBorder(
@@ -28,6 +85,7 @@ class SignupScreen extends StatelessWidget {
             const SizedBox(height: 20),
 
             TextField(
+              controller: emailController,
               decoration: InputDecoration(
                 hintText: "Email",
                 border: OutlineInputBorder(
@@ -39,6 +97,7 @@ class SignupScreen extends StatelessWidget {
             const SizedBox(height: 20),
 
             TextField(
+              controller: passwordController,
               obscureText: true,
               decoration: InputDecoration(
                 hintText: "Password",
@@ -51,6 +110,7 @@ class SignupScreen extends StatelessWidget {
             const SizedBox(height: 20),
 
             TextField(
+              controller: confirmPasswordController,
               obscureText: true,
               decoration: InputDecoration(
                 hintText: "Confirm Password",
@@ -65,21 +125,15 @@ class SignupScreen extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               height: 50,
+
               child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginScreen(),
-        ),
-      );
-    },
+                onPressed: signUpUser,
                 child: const Text(
-                  "Create Account",
+                  "Sign Up",
                   style: TextStyle(fontSize: 18),
-    ),
-  ),
-)
+                ),
+              ),
+            ),
 
           ],
         ),
